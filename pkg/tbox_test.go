b979f6369458eba2e55b391cd0b2a8616c0f81a1
Benjamin Ramser <legionaerr@googlemail.com>
2021-07-06 21:13:41 +0200

    test: 🚨 tile to bounding box

--- pkg/tbox_test.go
+++ pkg/tbox_test.go
@@ -74,6 +74,7 @@ func TestPointInTile(t *testing.T) {
 		{lat: 41.84, lng: -87.65, z: 3, x: 2, y: 2, expected: true},
 		{lat: 52.44950563632098, lng: 13.357951727129988, z: 15, x: 17599, y: 10756, expected: true},
 		{lat: 55.676098, lng: 12.568337, z: 11, x: 1095, y: 641, expected: true},
+		{lat: 52.25, lng: 13.37, z: 11, x: 111, y: 111, expected: false},
 	}
 
 	var p Point
@@ -85,8 +86,36 @@ func TestPointInTile(t *testing.T) {
 		p = Point{Lat: test.lat, Lng: test.lng}
 		i, _ = p.InTile(tile)
 
-		if !i {
+		if i != test.expected {
 			t.Errorf("Expected: %v, got: %v", test.expected, i)
+		}
+	}
+}
+
+func TestTileToBox(t *testing.T) {
+	var tests = []struct {
+		z      int
+		x      int
+		y      int
+		minLng float64
+		minLat float64
+		maxLng float64
+		maxLat float64
+	}{
+		{z: 11, x: 525, y: 761, minLng: -87.71484375, minLat: 41.77131167976407, maxLng: -87.5390625, maxLat: 41.9022770409637},
+		{z: 15, x: 17599, y: 10756, minLng: 13.348388671875, minLat: 52.44931414086969, maxLng: 13.359375, maxLat: 52.456009392640745},
+		{z: 11, x: 1095, y: 641, minLng: 12.48046875, minLat: 55.57834467218205, maxLng: 12.65625, maxLat: 55.67758441108952},
+	}
+
+	var tile Tile
+	var tbox Tilebox
+
+	for _, test := range tests {
+		tile = Tile{Z: test.z, X: test.x, Y: test.y}
+		tbox = tile.ToBox()
+
+		if tbox.MaxLat != test.maxLat || tbox.MaxLng != test.maxLng || tbox.MinLat != test.minLat || tbox.MinLng != test.minLng {
+			t.Errorf("Expected: %v, got: %v", Tilebox{MinLng: test.minLng, MinLat: test.minLat, MaxLng: test.maxLng, MaxLat: test.maxLat}, tbox)
 		}
 	}
 }
